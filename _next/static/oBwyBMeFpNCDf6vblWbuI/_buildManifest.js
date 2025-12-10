d9fc4906b5ae77880e8b3e9752b3343a56473b6f
Esteban Bravo <esteban.bravo2101@alumnos.ubiobio.cl>
2025-12-10 02:10:26 -0300

    Updates

--- _next/static/oBwyBMeFpNCDf6vblWbuI/_buildManifest.js
+++ _next/static/oBwyBMeFpNCDf6vblWbuI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portafolio-2.0//_next/:path+",
+        "destination": "/Portafolio-2.0/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
